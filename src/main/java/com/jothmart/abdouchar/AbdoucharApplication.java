f3ff3012e58a0c03c87c0764267d9e3ba7404e53
jothmani <jo.tlaesus.it@gmail.com>
2022-06-25 20:38:46 +0200

    Initial commit

---
+++ src/main/java/com/jothmart/abdouchar/AbdoucharApplication.java
@@ -0,0 +1,13 @@
+package com.jothmart.abdouchar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbdoucharApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbdoucharApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jothmart/abdouchar/AbdoucharApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jothmart.abdouchar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbdoucharApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
